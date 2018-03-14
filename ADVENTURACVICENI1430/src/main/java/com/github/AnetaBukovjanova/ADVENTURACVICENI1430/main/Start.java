91c273246b22b22d3711451202211db93f195383
AnetaBukovjanova <anetbukovjanova@gmail.com>
2018-03-14 23:12:35 +0100

    ---

---
+++ ADVENTURACVICENI1430/src/main/java/com/github/AnetaBukovjanova/ADVENTURACVICENI1430/main/Start.java
@@ -0,0 +1,61 @@
+/* Soubor je ulozen v kodovani UTF-8.
+ * Kontrola kódování: Příliš žluťoučký kůň úpěl ďábelské ódy. */
+package com.github.AnetaBukovjanova.ADVENTURACVICENI1430.main;
+
+
+
+import com.github.AnetaBukovjanova.ADVENTURACVICENI1430.logika.*;
+import com.github.AnetaBukovjanova.ADVENTURACVICENI1430.ui.HomeController;
+import com.github.AnetaBukovjanova.ADVENTURACVICENI1430.ui.TextoveRozhrani;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/*******************************************************************************
+ * Třída {@code Start} je hlavní třídou projektu,
+ * který ...
+ *
+ * @author    Filip Vencovský
+ * @version   0.00.000
+ */
+public class Start extends Application
+{
+    /***************************************************************************
+     * Metoda, prostřednictvím níž se spouští celá aplikace.
+     *
+     * @param args Parametry příkazového řádku
+     */
+    public static void main(String[] args)
+    {
+//		  TODO parametrické spuštění hry
+//        IHra hra = new Hra();
+//        TextoveRozhrani ui = new TextoveRozhrani(hra);
+//        ui.hraj();
+    	
+    	launch(args);
+    }
+    
+    /**
+	 * Metoda, ve které se konstruuje okno, kontroler a hra,
+	 * která se předává kontroleru
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+    	loader.setLocation(getClass().getResource("../ui/MainWindow.fxml"));    	
+    	Parent root = loader.load();
+
+ //		TODO předání hry kontroleru
+//    	HomeController controller = loader.getController();
+//    	IHra hra = new Hra();
+//		controller.inicializuj(hra);
+    	
+    	primaryStage.setScene(new Scene(root));
+    	primaryStage.show();
+    	primaryStage.setTitle("Základní adventura");
+		
+	}
+}
---
+++ ADVENTURACVICENI1430/src/main/java/com/github/AnetaBukovjanova/ADVENTURACVICENI1430/ui/HomeController.java
@@ -0,0 +1,28 @@
+package com.github.AnetaBukovjanova.ADVENTURACVICENI1430.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+/**
+ * Kontroler, který zprostředkovává komunikaci mezi grafikou
+ * a logikou adventury
+ * 
+ * @author Filip Vencovsky
+ *
+ */
+public class HomeController extends GridPane {
+	
+	@FXML private TextField textVstup;
+	
+	/**
+	 * Metoda čte příkaz ze vstupního textového pole
+	 * a zpracuje ho...
+	 */
+	public void odesliPrikaz() {
+//		TODO zpracovat příkaz a vepsat výsledek do výstupní oblasti
+		System.out.println(textVstup.getText());
+		
+	}
+
+}
